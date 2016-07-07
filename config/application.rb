5461408565c5c4c667375f468e1d5bace384297e
nyamada <n-yamada@rworks.jp>
2016-07-07 10:04:45 +0000

    Implementation of the validation

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,19 @@ module MessageBoard
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      if instance.kind_of?(ActionView::Helpers::Tags::Label)
+        # skip when label
+        html_tag.html_safe
+      else
+        # エラーメッセージを入力欄直下に表示する
+        # method_name = instance.instance_variable_get(:@method_name)
+        # errors = instance.object.errors[method_name]
+        # "<div class=\"has-error\">#{html_tag}<span class=\"help-block\">#{errors.first}</span></div>".html_safe
+        
+        "<div class=\"has-error\">#{html_tag}</div>".html_safe
+      end
+    end
   end
 end
